cd39eb71a3b96046fcc8a4bfe46941b7a46067c4
tp-chrisps <0pahlc05@solent.ac.uk>
2025-10-08 09:49:10 +0100

    arrays using numpy

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+employees = np.array(["John Stevenson",
+"Jane Smith",
+"Tim Wilson",
+"Kate Stevenson",
+"Kate Palmer",
+"Tom Eastman",
+"Laura Green",
+"Mike Watson",
+"Sally Black",
+"Mark Ramsey"])
+
+print(employees[1])
+print(employees[7])
