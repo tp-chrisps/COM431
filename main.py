c4de408a92e300b155750a3c1172b4c2468f2039
tp-chrisps <0pahlc05@solent.ac.uk>
2025-10-08 10:31:28 +0100

    Storing employees using a list instead of arrays

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-people = [None] * 3
+employees = ["John Stevenson",
+"Jane Smith",
+"Tim Wilson",
+"Kate Stevenson",
+"Kate Palmer",
+"Tom Eastman",
+"Laura Green",
+"Mike Watson",
+"Sally Black",
+"Mark Ramsey"]
 
-people[0] = input("enter person 1")
-people[1] = input("enter person 2")
-people[2] = input("enter person 3")
-print(people)
-people.append("james")
-print(people)
+print(employees[1])
+print(employees[7])
+
+employees.append("Kevin Kennedy")
+employees.append("Lisa Lord")
+print(employees)
